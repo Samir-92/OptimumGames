578a3f882e6cf1db1df5425b12dd146a62ee70b3
samir kouider-aissa <skouid10@caledonian.ac.uk>
2013-02-01 14:03:26 +0000

    first commit
    added project

---
+++ Assets/Scripts/Objects/RPGScript.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+public class RPGScript : MonoBehaviour {
+	
+	public GameObject bl;
+	
+	public float timeToSpawn = 1.0f;
+    public float currTime = 0.0f;
+	public GameObject[] ig;
+	
+	// Use this for initialization
+	void Start () {
+	     
+			 ig = GameObject.FindGameObjectsWithTag("Z");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		currTime += Time.deltaTime;
+		
+		 if (GameObject.FindWithTag("Z") != null)
+		{
+			if (currTime > timeToSpawn)
+	        {
+	            Instantiate(bl,transform.position, transform.rotation);
+				currTime = 0.0f;
+			}
+		}
+		else{
+  
+		foreach(GameObject go in ig)
+		{
+   		 if(go.gameObject.active == false)
+				{
+					go.gameObject.active = true;
+				}
+		}
+		}
+	}
+}
